999c89e878cb410ec0d6bc475d5fa79eba3532e8
progressh <yurui1201@gmail.com>
2026-05-07 10:33:48 -0400

    Update example.py

--- src/example.py
+++ src/example.py
@@ -1,1 +1,16 @@
+from cgi import parse_header, parse_multipart
+import cgi
+from urllib.parse import parse_qs
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os.path
+import subprocess,os,atexit,sys
+from shutil import copyfile
+import glob
+import json
+
+class RequestHandler(SimpleHTTPRequestHandler):
+
+os.chdir(str(gv_home_dir)+"/home")
+server = HTTPServer(('', 8000), RequestHandler)
+server.serve_forever()
 #iterate array
